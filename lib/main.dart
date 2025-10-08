ed63aeb455b2047fcf1b75aa515bdfab9a9f9a77
ahmedmostafa361 <ahmedmostafamm878@gmail.com>
2025-10-08 23:34:41 +0300

    + fire base

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseFirestore.instance.disableNetwork();
-  rrunApp(
+  runApp(
       MultiProvider(
           providers: [
             ChangeNotifierProvider(
